cc0deca5e8b8ee07e72d0491b5b485ce7f9669a8
Seyed Mahdi Hosseini <smhosseini96@outlook.com>
2022-10-24 16:19:07 +0330

    Added Animation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -182,3 +182,19 @@ const scrollHeader = () =>{
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  reset: true //Animation Repeat
+})
+
+sr.reveal(`.home__data, .projects__container, .testimonial__container, footer__container`)
+sr.reveal(`.home__info div`, {delay: 600, origin: 'bottom', interval: 100})
+sr.reveal(`.skills__content:nth-child(1), .contact__content:nth-child(1)`, {origin: 'left'})
+sr.reveal(`.skills__content:nth-child(2), .contact__content:nth-child(2)`, {origin: 'right'})
+sr.reveal(`.qualification__content, .services__card`, {interval: 100})
+
+
